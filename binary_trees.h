7cbdd10c3288f3f4172aba8a4de379263e1600a3
sara0411 <saraferraa0@gmail.com>
2023-11-03 01:21:25 +0100

    task 15

--- binary_trees.h
+++ binary_trees.h
@@ -58,6 +58,7 @@ size_t binary_tree_size(const binary_tree_t *tree);
 
 size_t binary_tree_leaves(const binary_tree_t *tree);
 
+size_t binary_tree_nodes(const binary_tree_t *tree);
 
 
 #endif /* _BINARY_TREES_H_ */
